7c4e94a1d4e904d767a3cfc95354e08dc4376e79
Hiroki Gota <hiroki.gota@spatialvision.com.au>
2016-04-07 14:23:09 +0800

    patch version

---
+++ update-version.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var pjson = require('./package.json');
+var xeditor = require("gulp-xml-editor");
+
+console.log('Updating connfig.xml version to ', pjson.version);
+
+
+gulp.src("./config.xml")
+  .pipe(xeditor(function(xml) {
+    var elem = xml.root();
+    elem.attr('version').value(pjson.version);
+    console.log('version updated to %s', elem.attr('version').value());
+    return xml;
+  }))
+  .pipe(gulp.dest("./"));
