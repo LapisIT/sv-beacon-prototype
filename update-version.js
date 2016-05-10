1af27991e57e4cc5dbbafcbb8a614c5da3a36182
hxg <hiroki.gota@spatialvision.com.au>
2016-05-10 14:12:56 +1000

    removed manifest copy

--- update-version.js
+++ update-version.js
@@ -14,11 +14,13 @@ gulp.src("./config.xml")
   }))
   .pipe(gulp.dest("./"));
 
+/*
 gulp.src("./AndroidManifest.xml")
   .pipe(xeditor(function(xml) {
     var elem = xml.root();
-    elem.attr('version').value(pjson.version);
+    elem.attr('android:versionName').value(pjson.version);
     console.log('version updated to %s', elem.attr('version').value());
     return xml;
   }))
   .pipe(gulp.dest("./"));
+  */
